41fc4cb8b0c10669051b5e4b52d420eb35b8b015
adcpm <fabien@bonustrack.co>
2018-10-16 18:58:04 +0700

    Init

---
+++ index.js
@@ -0,0 +1,46 @@
+const Mnemonic = require('bitcore-mnemonic');
+const objectHash = require('byteballcore/object_hash');
+
+const generateRandomSeed = () => {
+  let mnemonic = new Mnemonic();
+  while (!Mnemonic.isValid(mnemonic.toString())) {
+    mnemonic = new Mnemonic();
+  }
+  return mnemonic.phrase;
+};
+
+const target = process.env.TARGET || 'FABIEN';
+const path = "m/44'/0'/0'/0/0";
+
+const generateAddress = () => {
+  const randomSeed = generateRandomSeed();
+  const mnemonic = new Mnemonic(randomSeed);
+  const xPrivKey = mnemonic.toHDPrivateKey();
+  const { privateKey } = xPrivKey.derive(path);
+  const pubkey = privateKey.publicKey.toBuffer().toString('base64');
+  const definition = ['sig', { pubkey }];
+  return {
+    seed: randomSeed,
+    address: objectHash.getChash160(definition),
+  };
+};
+
+let address;
+let seed;
+
+do {
+  const generated = generateAddress();
+  address = generated.address;
+  seed = generated.seed;
+  if (address.slice(0, 3) === target.slice(0, 3)) {
+    console.log('3', address, seed);
+  }
+  if (address.slice(0, 4) === target.slice(0, 4)) {
+    console.log('4', address, seed);
+  }
+  if (address.slice(0, 5) === target.slice(0, 5)) {
+    console.log('5', address, seed);
+  }
+} while (address.slice(0, target.length) !== target);
+
+console.log('Bingo!', address, seed);
